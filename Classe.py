f974742401cbe26d0f55f4cc6ed28186bceb9202
AMA <welliton.henrique.fermino17@gmail.com>
2021-10-21 17:00:20 -0300

    Criei Classe

---
+++ Classe.py
@@ -0,0 +1,12 @@
+class Pessoa:
+    #construtor da classe
+    def __init__(self):
+        self.a = "João"
+        self.b = "Maria"
+        self.c = "Carro"
+
+    def Funcao(self):
+        return "Hoje está ensolarado! :)"
+
+    def W(self):
+        return "Olá como vai"
---
+++ ImportacaoClasse.py
@@ -0,0 +1,5 @@
+from Classe import Pessoa 
+
+t = Pessoa()
+
+print (t.Funcao())
